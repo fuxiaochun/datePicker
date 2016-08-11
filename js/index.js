b840f7cd3e78a9039aedf3905efc4ac3b9487850
xiaochun <cqouting@sina.com>
2016-08-11 18:58:49 +0800

    进行中

--- js/index.js
+++ js/index.js
@@ -33,21 +33,31 @@ function getWeekTpl() {
 	return html;
 }
 
-function getFirstDay(y, m) {
-	return new Date(y, m, 1).getDay();
-}
-
-function getLastDay(y, m, d) {
+function getTheDay(y, m, d) {
 	return new Date(y, m, d).getDay();
 }
 
+function makeData(s, c, e) {
+	var total = s + c + e;
+}
+
 function getDays(timeStamp) {
-	var date = typeof timeStamp === 'undefind' ? new Date() : new Date(timeStamp);
+
+	var date = typeof timeStamp === 'undefined' ? new Date() : new Date(timeStamp);
 	var year = date.getFullYear();
 	var month = date.getMonth() + 1;
+
+	// 本月和上下月的每月总天数
 	var curMonthDays = getDaysEveryMonth(year, month, 0);
 	var lastMonthDays = getDaysEveryMonth(year, month - 1, 0);
 	var nextMonthDays = getDaysEveryMonth(year, month + 1, 0);
+
+	// 当月前面空的天数
+	var startDays = getTheDay(year, month - 1, 1);
+	var endDays = 6 - getTheDay(year, month - 1, curMonthDays);
+
+	var daysData = makeData(startDay, curMonthDays, endDay);
+
 
 }
 
@@ -55,7 +65,7 @@ function initHTML() {
 	var curYear = getDates().year;
 	var curMonth = getDates().month;
 	var weekTpl = getWeekTpl();
-	var datesTpl = getDates();
+	var datesTpl = getDays();
 
 	var $datePicker = $('<div class="dp-wrap" id="varfn-dp"><div class="dp-menu"><div class="dp-nav" id="varfn-lastY" title="上一年">&lt;&lt;</div><div class="dp-nav" id="varfn-lastM" title="上一月">&lt;</div><div class="ym"><span class="y" id="varfn-dp-y">' + curYear + '</span> 年 <span class="m" id="varfn-dp-m">' + curMonth + '</span> 月 </div><div class="dp-nav" id="varfn-nextM" title="下一月">&gt;</div><div class="dp-nav" id="varfn-nextY" title="下一年">&gt;&gt;</div></div><div class="day" id="varfn-dp-day">' + weekTpl + '</div><ul class="date" id="varfn-dp-date">' + datesTpl + '</ul></div>');
 	$('body').append($datePicker);
